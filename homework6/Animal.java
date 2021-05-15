63be2338e3118f719cd7615aaa81f5ecafe112b3
smugallimov <smugallimov@mail.ru>
2021-05-15 09:35:48 +0400

    Home Work #6.

---
+++ homework6/Animal.java
@@ -0,0 +1,49 @@
+package homework6;
+
+public class Animal {
+    public void voice() {
+    }
+    public String name(){
+        return " ";
+    }
+}
+class Dog extends Animal{
+    @Override
+    public void voice(){
+    System.out.println("WOOF!! -- WOOF!!");
+    }
+    @Override
+    public String name(){
+        return "Dog";
+    }
+}
+class Cat extends Animal{
+    @Override
+    public void voice(){
+        System.out.println("MEU!! -- MEU!!");
+    }
+    @Override
+    public String name(){
+        return "Cat";
+    }
+}
+class Fish extends Animal{
+    @Override
+    public void voice(){
+        System.out.println("SPLASH!! -- SPLASH!!");
+    }
+    @Override
+    public String name(){
+        return "Fish";
+    }
+}
+class Bird extends Animal{
+    @Override
+    public void voice(){
+        System.out.println("CAW!! -- CAW!!");
+    }
+    @Override
+    public String name(){
+        return "Bird";
+    }
+}
---
+++ homework6/AnimalShaker.java
@@ -0,0 +1,14 @@
+package homework6;
+
+public class AnimalShaker {
+    public static void main(String[] args) {
+        Animal[] animalArray = new Animal[]{new Dog(), new Cat(), new Fish(), new Bird()};
+        for(Animal animal: animalArray){
+            makeVoice(animal);
+        }
+    }
+    private static void makeVoice(Animal animal){
+        System.out.print(animal.name() + " is making: ");
+        animal.voice();
+    }
+}
