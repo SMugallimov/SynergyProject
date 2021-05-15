f7a63f2456b56af4799b530d36c99e2ea4252a94
smugallimov <smugallimov@mail.ru>
2021-05-15 09:34:35 +0400

    Home Work #5.

---
+++ homework5/Matrix.java
@@ -0,0 +1,137 @@
+package homework5;
+
+public class Matrix {
+    private int rowLength;
+    private int colLength;
+    private int[][] array;
+
+    public int getRowLength() {
+        return rowLength;
+    }
+    public int getColLength() {
+        return colLength;
+    }
+
+    public Matrix(){
+        this.rowLength = 2;
+        this.colLength = 2;
+        this.array = new int[rowLength][colLength];
+    }
+
+    public Matrix(int rowLength, int colLength){
+        this.rowLength = rowLength;
+        this.colLength = colLength;
+        this.array = new int[rowLength][colLength];
+    }
+
+    public Matrix addition(Matrix matrix){
+        if(getRowLength() != getColLength()){
+            return null;
+        }
+        Matrix result = new Matrix(getRowLength(), getColLength());
+        for(int i = 0; i < result.getRowLength(); i++){
+            for(int j = 0; j < result.getColLength(); j++){
+                result.array[i][j] = this.array[i][j] + matrix.array[i][j];
+            }
+        }
+        return result;
+    }
+
+    public Matrix subtraction(Matrix matrix){
+        if(getRowLength() != getColLength()) {
+            return null;
+        }
+        Matrix result = new Matrix(getRowLength(), getColLength());
+        for(int i = 0; i < result.getRowLength(); i++){
+            for(int j = 0; j < result.getColLength(); j++){
+                result.array[i][j] = this.array[i][j] - matrix.array[i][j];
+            }
+        }
+        return result;
+    }
+
+    public Matrix multiply(Matrix matrix1, Matrix matrix2){
+        Matrix result = new Matrix(matrix1.getRowLength(), matrix2.getColLength());
+        for(int i = 0; i < matrix1.getRowLength(); i++){
+            for(int j = 0; j < matrix2.getColLength(); j++){
+                for(int k = 0; k < matrix1.getColLength(); k++){
+                    result.array[i][j] += matrix1.array[i][k] * matrix2.array[k][j];
+                }
+            }
+        }
+        return result;
+    }
+
+    public static Matrix reverse(Matrix matrix){
+        Matrix result = new Matrix();
+        for(int i = 0; i < matrix.getRowLength(); i++){
+            for(int j = 0; j < matrix.getColLength(); j++){
+                result.array[i][j] = matrix.array[j][i];
+            }
+        }
+        return result;
+    }
+
+    public Matrix reverse(){
+        Matrix result = new Matrix();
+        for(int i = 0; i < result.getRowLength(); i++){
+            for(int j = 0; j < result.getColLength(); j++){
+                result.array[i][j] = this.array[j][i];
+            }
+        }
+        return result;
+    }
+
+    public static Matrix unitMatrix(int size){
+        Matrix result = new Matrix(size, size);
+        for(int i = 0; i < result.getRowLength(); i++){
+            for(int j = 0; j < result.getColLength(); j++){
+                if(i == j){
+                    result.array[i][j] = 1;
+                }
+                else{
+                    result.array[i][j] = 0;
+                }
+            }
+        }
+        return result;
+    }
+
+    public Matrix matrixMultiOnNumber(Matrix matrix, int number){
+        Matrix result = new Matrix(getRowLength(), getColLength());
+        for(int i = 0; i < matrix.getRowLength(); i++){
+            for(int j = 0; j < matrix.getColLength(); j++){
+                result.array[i][j] = number * matrix.array[i][j];
+            }
+        }
+        return result;
+    }
+
+    public Matrix powMatrix(Matrix matrix, int n){
+        Matrix result = matrix;
+        for(int i = 1; i < n; i++){
+            result = multiply(result, matrix);
+        }
+        return result;
+    }
+
+    public void fillMatrix(){
+        int[][] array = new int[getRowLength()][getColLength()];
+        int counter = 0;
+        for(int i = 0; i < getRowLength(); i++){
+            for(int j = 0; j < getColLength(); j++){
+                this.array[i][j] = counter;
+                counter++;
+            }
+        }
+    }
+
+    public void printMatrix(){
+        for(int i = 0; i < getRowLength(); i++){
+            for(int j = 0; j < getColLength(); j++){
+                System.out.printf(" %d", this.array[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ homework5/MatrixLauncher.java
@@ -0,0 +1,48 @@
+package homework5;
+
+public class MatrixLauncher {
+    public static void main(String[] args) {
+        System.out.println("Matrix #1:");
+        Matrix matrix1 = new Matrix();
+        matrix1.fillMatrix();
+        matrix1.printMatrix();
+
+        System.out.println("Matrix #2:");
+        Matrix matrix2 = new Matrix(4,4);
+        matrix2.fillMatrix();
+        matrix2.printMatrix();
+
+        Matrix result = new Matrix();
+        System.out.println("Addition:");
+        result = matrix1.addition(matrix1);
+        result.printMatrix();
+
+        System.out.println("Subtraction:");
+        result = matrix1.subtraction(matrix2);
+        result.printMatrix();
+
+        System.out.println("Multiplication:");
+        result = result.multiply(matrix1, matrix2);
+        result.printMatrix();
+
+        System.out.println("Static revers with parameters:");
+        result = matrix1.reverse(matrix1);
+        result.printMatrix();
+
+        System.out.println("Revers w/o parameters:");
+        result = matrix1.reverse();
+        result.printMatrix();
+
+        System.out.println("Unit matrix:");
+        result = matrix2.unitMatrix(4);
+        result.printMatrix();
+
+        System.out.println("Multiply on number:");
+        result = matrix2.matrixMultiOnNumber(matrix2, 4);
+        result.printMatrix();
+
+        System.out.println("Power of matrix:");
+        result = matrix1.powMatrix(matrix1, 2);
+        result.printMatrix();
+    }
+}
