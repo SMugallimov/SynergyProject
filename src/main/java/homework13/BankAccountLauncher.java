e6da33832734c4759640f89ce69c865c708a8248
smugallimov <smugallimov@mail.ru>
2021-07-05 15:16:59 +0500

    Home Work #13: Reflection API. Updated

--- src/main/java/homework13/BankAccountLauncher.java
+++ src/main/java/homework13/BankAccountLauncher.java
@@ -4,16 +4,26 @@ import java.lang.reflect.Field;
 import java.math.BigDecimal;
 
 public class BankAccountLauncher {
-    public static void main(String[] args) throws Exception {
-        try {
-            IndividualsBankAccount account = new IndividualsBankAccount("Client1", 1234, BigDecimal.valueOf(100));
-            account.deposit(BigDecimal.valueOf(10_000.00));
-            account.withdrawal(BigDecimal.valueOf(10_000.00));
-        } catch (NotEnoughMoneyException ex) {
-            System.out.println(ex.getMessage());
-        }
 
-        Field individual = BankAccount.class.getField("Client1");
-        System.out.println(individual);
+    public static void main(String[] args) throws Exception {
+
+//        try {
+//            IndividualsBankAccount account = new IndividualsBankAccount("Client1", 1234,
+//                    BigDecimal.valueOf(100));
+//            account.deposit(BigDecimal.valueOf(10_000.00));
+//            account.withdrawal(BigDecimal.valueOf(10_000.00));
+//        } catch (NotEnoughMoneyException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+        Class<IndividualsBankAccount> individualsBankAccountClass = IndividualsBankAccount.class;
+        Field[] individualsFields = individualsBankAccountClass.getDeclaredFields();
+        for(Field field: individualsFields){
+            System.out.println(field);
+        }
+        Class<LegalsBankAccount> legalsBankAccountClass = LegalsBankAccount.class;
+        Field[] legalFields = legalsBankAccountClass.getDeclaredFields();
+        for(Field field: legalFields){
+            System.out.println(field);
+        }
     }
 }
