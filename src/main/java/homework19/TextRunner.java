5a96cbdb0b67180eb491e6a51d62c264553bd41d
smugallimov <smugallimov@mail.ru>
2021-08-01 15:52:59 +0500

    Home Work #19: Stream API_Corrected

--- src/main/java/homework19/TextRunner.java
+++ src/main/java/homework19/TextRunner.java
@@ -1,16 +1,17 @@
 package homework19;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class TextRunner {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         Map<String, Integer> map = Arrays.stream(
-                in.nextLine().replace('.', ' ').replace(',', ' ')
+                in.nextLine().toLowerCase().replace('.', ' ').replace(',', ' ')
                         .split("\\s+"))
 
-                .collect(Collectors.toMap(k -> k, k -> 1, Integer::sum))
+                .collect(Collectors.toMap(Function.identity(), k -> 1, Integer::sum))
                 .entrySet().stream()
                 .sorted(Comparator
                         .comparingInt(e -> -(int) ((Map.Entry) e).getValue())
