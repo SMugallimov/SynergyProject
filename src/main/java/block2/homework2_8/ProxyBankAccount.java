c0e5e902037f39471ed10c5fd82cb5bd8b5596fa
smugallimov <smugallimov@mail.ru>
2021-08-10 18:55:33 +0500

    Homework #2_8: @Blocked, Reflection API, Proxy-Objects

--- src/main/java/block2/homework2_8/ProxyBankAccount.java
+++ src/main/java/block2/homework2_8/ProxyBankAccount.java
@@ -2,6 +2,8 @@ package block2.homework2_8;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 public class ProxyBankAccount implements InvocationHandler {
     public BankAccount bankAccount;
@@ -11,16 +13,23 @@ public class ProxyBankAccount implements InvocationHandler {
     }
 
     @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable{
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+
+//        Method originMethod = bankAccount.getClass().getMethod(method.getName(),
+//                Stream.of(args).map(Object::getClass).toArray(Class<?>[]::new));
+//            if (originMethod.isAnnotationPresent(Blocked.class)) {
+//                throw new BlockedTransactions("Transaction is blocked");
+//            }
 
         Class clazz = BankAccount.class;
-        Method[] methods = clazz.getMethods();
-
-        for(Method m : methods){
-            if (m.isAnnotationPresent(Blocked.class)) {
+        Method[] originMethods = clazz.getMethods();
+        for(Method m: originMethods){
+            if(m.getName().equals("deposit") && m.isAnnotationPresent(Blocked.class) || m.getName().equals("withdrawal")
+                    && m.isAnnotationPresent(Blocked.class)) {
                 throw new BlockedTransactions("Transaction is blocked");
             }
         }
+
         return  method.invoke(bankAccount, args);
     }
 }
