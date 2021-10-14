a9aa9876cc56845028c742d82c607d997cbea771
smugallimov <smugallimov@mail.ru>
2021-10-14 17:33:57 +0400

    Homework #4.3 JSON Parsing example

--- src/main/java/block4/homework4_3/WeatherRunner.java
+++ src/main/java/block4/homework4_3/WeatherRunner.java
@@ -5,7 +5,7 @@ import java.net.URL;
 public class WeatherRunner {
     public static final String WEATHER_URL =
             "http://api.openweathermap.org/data/2.5/weather?q=Moscow, ru" +
-                    "&units=metric&appid=3012510208027f9d646e63e490eb8e10";
+                    "&units=metric&appid=xxxx";
 
     public static void main(String[] args) {
         URL url = JsonWeatherUtils.createUrl(WEATHER_URL);
